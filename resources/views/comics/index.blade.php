3a3ecaafb0e771b930f10808e4a3b0091221daa9
danieleangeletti <angelettidaniele@gmail.com>
2024-02-29 17:38:25 +0100

    pagina index completata

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,7 +5,26 @@
 @section('main-content')
     <main>
         <div class="container">
-            MAIN
+            <div class="row">
+                @foreach ($comics as $index => $comic)
+                    <div class="col-3 pb-3">
+                        <div class="card h-100" style="width: 18rem;">
+                            <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$comic->title}}</h5>
+                                <p class="card-text">{{$comic->description}}</p>
+                                <p class="card-text">{{$comic->price}}</p>
+                                <p class="card-text">{{$comic->series}}</p>
+                                <p class="card-text">{{$comic->sale_date}}</p>
+                                <p class="card-text">{{$comic->type}}</p>
+                                <p class="card-text">Artists: {{$comic->artists}}</p>
+                                <p class="card-text">Writers: {{$comic->writers}}</p>
+                                <a href="#" class="btn btn-primary">SHOW</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </main>
 @endsection
